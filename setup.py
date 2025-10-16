a195e07d88bedb0aafc381936425768d33442c52
panda-cat <90021060+panda-cat@users.noreply.github.com>
2025-10-16 09:52:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """Build a standalone executable using cx_freeze"""
+
+from cx_Freeze import setup, Executable
+
+setup(
+    name = "myapp",
+    version = "0.1",
+    description = "Connect to network devices and execute commands using netmiko",
+    executables = [Executable("myapp_scripts.py")],
+    platforms=["win32", "win64", "macosx", "linuxx86_64"],
+    packages=['netmiko', 'openpyxl', 'argparse', 'os', 'datetime', 'sys', 're', 'uuid', 'tqdm', 'paramiko==3.5.0'],
+)
